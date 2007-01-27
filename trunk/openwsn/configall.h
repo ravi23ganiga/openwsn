a8cb78ac22333ce3d700968f9e1a34cad98a79a5
openwsn <openwsn@06698cb7-0328-0410-9363-87a36b743fc7>
2007-01-27 12:02:36 +0000

    the first time to submit source codes

---
+++ trunk/openwsn/configall.h
@@ -0,0 +1,117 @@
+#ifndef _CONFIGALL_H_4237_
+#define _CONFIGALL_H_4237_
+
+/*******************************************************************************
+ * @author zhangwei on 2006-07-20
+ * this is the configure file of the whole project
+ * you can #define or #undef the macros in this file to enable/disable some 
+ * software modules.
+ * 
+ * every module including re-configure functions SHOULD include this file as 
+ * their first one. this is implemented by including "foundation.h". the "foundation.h"
+ * has already included the "configall.h"
+ * 
+ * @modified by zhangwei on 2006-07-20
+ * @modified by zhangwei on 20061015
+ * add macro CONFIG_MAX_FRAME_LENGTH
+ * 
+ * @modified by zhangwei on 20061-26
+ * add macros to support different hardware platforms
+ ******************************************************************************/
+ 
+
+/*******************************************************************************
+ * @attention
+ *                        IMPORTANT ISSUE ON HOW TO USE THIS FILE
+ *
+ * if you want to cancel some macro, pls use #undef macro. you can just place
+ * the #undef after the previous macro. if you want it to take effective, you 
+ * can place the #undef XXX macro before the configuration macro.
+ * 
+ * NOT to use "//" to comment these macros. because future developes may think 
+ * they are really comments and deleted them! 
+ ******************************************************************************/
+ 
+#define FOR_2420_TEST 
+ 
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 2
+ 
+/* a macro to enable the debug source code 
+ * for release version, you should undef this macro
+ */
+#undef GDEBUG
+#define GDEBUG
+
+/* Hardware Platform Choosing Configuration
+ * now we have four hardware platforms:
+ * - OpenNODE version 1.0
+ * - OpenNODE version 2.0
+ * - WlsModem version 1.1
+ * - GAINS
+ */
+#define TARGET_OPENNODE_10
+#undef TARGET_OPENNODE_10
+
+#undef TARGET_OPENNODE_20
+#define TARGET_OPENNODE_20
+
+
+#define TARGET_WLSMODEM_11
+#undef TARGET_WLSMODEM_11
+
+#define TARGET_GAINS 
+#undef TARGET_GAINS 
+
+
+ 
+
+/* FlashStore的开始地址和大小
+ * FlashStore用于系统掉电期间存储各种设置参数,它是MCU芯片E2PROM或者Flash地址空间中的一片
+ * 区域. 这两个参数用于初始化TConfigure对象(service\configure.*)
+ * @warning
+ * - 由于系统程序也被烧制在Flash中,所以用于保存系统配置信息的FlashStore区域必须避开程序
+ * 所在区域.
+ * - FlashStore的size必须比系统配置数据实际占用空间大,否则将发生越界错误
+ */
+#define CONFIG_FLASHSTORE_ADDR 0x4215
+#define CONFIG_FLASHSTORE_SIZE sizeof(TConfigStore)
+
+/* 该宏控制系统多长时间必须喂狗一次
+ * 该数值如何解释,取决于watchdog硬件和软件要求
+ */
+#define CONFIG_WATCHDOG_INTERVAL  50000
+
+/* 串口SIO Frame的开始标志字节
+ * 对流式Modem而言，不使用该标志
+ */
+#define CONFIG_START_OF_FRAME 27
+
+/* UART匹配采用RS232还是485
+ */
+#undef CONFIG_UART_RS232
+#define CONFIG_UART_RS232
+
+#define CONFIG_UART_RS485
+#undef CONFIG_UART_RS485
+
+#ifdef CONFIG_UART_RS232
+  #undef CONFIG_UART_RS485
+#endif  
+
+/* If you want the application to support RTOS like uCOS, you should define the following 
+ * macro. the default setting is RTOS enabled */
+
+#define CONFIG_USE_RTOS  
+#undef CONFIG_USE_RTOS
+
+/* maximum frame length. no more than 127*/
+#define CONFIG_MAX_PHY_FRAME_LENGTH 0x7F
+#define CONFIG_MAX_MAC_FRAME_LENGTH (CONFIG_MAX_PHY_FRAME_LENGTH-9)
+#define CONFIG_MAX_UART_FRAME_LENGTH 0x7F
+
+/* UART frame identification byte */
+#define CONFIG_DEFAULT_ESCAPE_CHAR 27
+#define CONFIG_DEFAULT_FRAME_PREFIX CONFIG_DEFAULT_ESCAPE_CHAR
+
+#endif
---
+++ trunk/openwsn/global.h
@@ -0,0 +1,81 @@
+#ifndef _GLOBAL_H_6382_
+#define _GLOBAL_H_6382_
+
+/*****************************************************************************
+ * This file is part of OpenWSN, the Open Wireless Sensor Network System.
+ *
+ * Copyright (C) 2005,2006,2007 zhangwei (openwsn@gmail.com)
+ * 
+ * OpenWSN is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 or (at your option) any later version.
+ * 
+ * OpenWSN is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with eCos; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * 
+ * As a special exception, if other files instantiate templates or use macros
+ * or inline functions from this file, or you compile this file and link it
+ * with other works to produce a work based on this file, this file does not
+ * by itself cause the resulting work to be covered by the GNU General Public
+ * License. However the source code for this file must still be made available
+ * in accordance with section (3) of the GNU General Public License.
+ * 
+ * This exception does not invalidate any other reasons why a work based on
+ * this file might be covered by the GNU General Public License.
+ * 
+ ****************************************************************************/ 
+
+//----------------------------------------------------------------------------
+// @author zhangwei on 2006-07-25
+// Global Variables with initialization and finalization
+//
+// 	This module is very important in the whole application. It does not only 
+// declares and initializes all the global objects, but also create the 
+// relationships between different objects.
+// 	In TinyOS and nesC systems, these relationships are constructed by a separate
+// Configurator object. As you have seem here, this is not necessary. You should
+// understand and adopt the OOP programming model and use it in such conditions.
+//  
+//----------------------------------------------------------------------------
+
+#include "foundation.h"
+#include ".\hal\hal.h"
+#include ".\service\svc_actsche.h"
+#include ".\service\svc_siocomm.h"
+#include ".\service\svc_wlscomm.h"
+#include ".\service\svc_console.h"
+#include ".\service\svc_debugio.h"
+#include ".\service\svc_configure.h"
+
+extern TActionScheduler * 	g_actsche;
+extern TSioComm *			g_sio;
+extern TWirelessComm *		g_wls;
+extern TConsole *			g_console;
+extern TConfigure *			g_config;
+extern TDebugIo * 			g_debugio;
+
+
+// construct all the necessary global object instances. The relationship between
+// all these objects are also created.
+// 
+// @return 
+//	0			success
+//	negative	failed
+//
+int8 global_construct( void );
+
+// destroy all the global object instances and their relationships.
+// 
+// @return 
+//	0			success
+//	negative	failed
+//
+int8 global_destroy( void );
+
+#endif
