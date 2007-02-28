d600d12ff0f34fc8591793d3bcdf4a38458985fa
openwsn <openwsn@06698cb7-0328-0410-9363-87a36b743fc7>
2007-02-28 02:21:15 +0000

    Copied remotely

---
+++ trunk/libsink-0.5.1/global.cpp
@@ -0,0 +1,3 @@
+#include "global.h"
+
+char * g_owner = "TongJi University, 2005-2007";
---
+++ trunk/libsink-0.5.1/service/svc.h
@@ -0,0 +1,10 @@
+
+#ifndef _SVC_H_4216_
+#define _SVC_H_4216_
+
+#include "svc_configall.h"
+#include "svc_foundation.h"
+#include "svc_console.h"
+#include "svc_siocomm.h"
+
+#endif
