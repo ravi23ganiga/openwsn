3d6ec0f89f4b0cf30a42f106fe5f548e6fa86a81
openwsn <openwsn@06698cb7-0328-0410-9363-87a36b743fc7>
2009-11-27 17:55:18 +0000

    openwsn-1.0rc (release candidate) compiled with avrstudio and winavr running on gainz

--- node/common/osx/osx_foundation.c
+++
@@ -1,7 +0,0 @@
-#include "osx_configall.h"
-#include "../hal/hal_timer.h"
-#include "../hal/hal_uart.h"
-#include "../hal/hal_interrupt.h"
-#include "../rtl/rtl_ringqueue.h"
-#include "osx_foundation.h"
-
--- openwsn/hal/hal_random.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HAL_RANDOM_H_3425_
-#define _HAL_RANDOM_H_3425_
-
-void randinit( uint16 seed );
-uint16 random();
-
-#endif
-
